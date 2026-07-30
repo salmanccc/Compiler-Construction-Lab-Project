0ec084d99520022107ab268e78e392402f8dc472
salmanccc <ahnn3200@gmail.com>
2026-07-30 09:09:41 -0700

    Implement basic code generation

---
+++ src/codegen/codegen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void generateAssignment(char var[], int value)
+{
+    printf("MOV %s, %d\n", var, value);
+}
+
+void generatePrint(char var[])
+{
+    printf("PRINT %s\n", var);
+}
+
+int main()
+{
+    printf("===== CODE GENERATION =====\n\n");
+
+    generateAssignment("age", 20);
+    generateAssignment("marks", 95);
+
+    generatePrint("age");
+    generatePrint("marks");
+
+    return 0;
+}
